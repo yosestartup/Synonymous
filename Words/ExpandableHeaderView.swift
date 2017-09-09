30dd7af3d8b5073ff17ed336adc15bbb23c9eaeb
Oleksandr Bambuliak <yosestartup@gmail.com>
2017-09-09 23:54:22 +0200

    Initial commit

---
+++ Words/ExpandableHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpandableHeaderView.swift
+//  
+//
+//  Created by Александр on 16.07.17.
+//
+//
+
+import UIKit
+
+class ExpandableHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Words/ScaleSegue.swift
@@ -0,0 +1,13 @@
+//
+//  ScaleSegue.swift
+//  Words
+//
+//  Created by Александр on 23.07.17.
+//  Copyright © 2017 hil. All rights reserved.
+//
+
+import UIKit
+
+class ScaleSegue: UIStoryboardSegue {
+
+}
